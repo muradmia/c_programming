bb052c9345ab6a6c80e72fbead899f4decc0166e
murad-mia <muradmia901@gmail.com>
2023-04-30 22:56:35 +0600

    module 14

---
+++ module 14/function_4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void add(){
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int sum = a + b;
+    printf("sum = %d\n",sum);
+}
+int main(){
+
+    add();
+
+
+    return 0;
+}
